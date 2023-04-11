b4c956e2061723fe91dfe99c9e05e14a0ec13b00
ShivanKunwar <109016623+Rugeno@users.noreply.github.com>
2023-04-11 22:47:19 +0545

    Client and connection
    Shifted frontend to clients folder and developed a connection string

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+const app = express()
+dotenv.config();
+
+
+const connect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB);
+        console.log("Connected to the database")
+      } catch (error) {
+        console.log(error);
+      }
+
+};
+
+  
+
+app.listen(8800,()=>{
+    connect()
+    console.log("Running Succesfully")
+})
